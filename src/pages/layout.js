b4d7ff797743528c58424fc7ce3b4eefeede93bc
Luna Alcantara <gabrielle.alcantara@sga.pucminas.br>
2024-04-28 19:30:02 -0300

    fix: :lipstick: update drawer navigation

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,31 +1,49 @@
 import React from "react";
+import { Appbar, useTheme } from "react-native-paper";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  DrawerActions,
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
+
 import ExamplePage from "./examplePage";
 import AnotherExample from "./anotherExample";
 
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-
 const Drawer = createDrawerNavigator();
+import CustomDrawerContent from "../components/customDrawerContent";
 
 const Layout = () => {
+  const navigationRef = useNavigationContainerRef();
+  const theme = useTheme();
+
   return (
-    <>
-      <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName="ExamplePage"
-          screenOptions={{
-            drawerInactiveTintColor: "#fff",
-            drawerActiveTintColor: "#fff",
-            drawerStyle: {
-              backgroundColor: "#1D2D3D",
-            },
+    <NavigationContainer ref={navigationRef}>
+      <Appbar.Header style={{ backgroundColor: theme.colors.primary }}>
+        <Appbar.Action
+          icon="menu"
+          color="#fff"
+          onPress={() => {
+            navigationRef.dispatch(DrawerActions.toggleDrawer());
           }}
-        >
-          <Drawer.Screen name="Example Page" component={ExamplePage} />
-          <Drawer.Screen name="Another Example" component={AnotherExample} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </>
+        />
+        <Appbar.Content title="Game of IT" color="white" />
+      </Appbar.Header>
+      <Drawer.Navigator
+        initialRouteName="ExamplePage"
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{
+          drawerType: "front",
+          headerShown: false,
+          drawerStyle: {
+            backgroundColor: "#1D2D3D",
+          },
+        }}
+      >
+        <Drawer.Screen name="Example Page" component={ExamplePage} />
+        <Drawer.Screen name="Another Example" component={AnotherExample} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
